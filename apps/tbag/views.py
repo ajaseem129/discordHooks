1d636f03c85358889db655cc955be5f88f0c4182
Drak <drak@Abduls-MacBook-Air.local>
2022-08-26 21:31:26 +0530

    Update dates

--- apps/tbag/views.py
+++ apps/tbag/views.py
@@ -10,7 +10,7 @@ from datetime import date
 # Create your views here.
 class Hook(APIView):
     def get(self,request):
-        dat =date(2022,12,15)- date.today()
+        dat =date(2022,12,17)- date.today()
         webhook = SyncWebhook.from_url("https://discordapp.com/api/webhooks/1012748651577217034/d08JuvqoKI_85AIm7faAfxHBwhuFFMCTIfdvX0rSP36fKsgFqjgo_wz4ebowQKmP5gyB")
         resp =webhook.send(f'Days to Bangalore Trip V4 : **{dat.days}**')
         return HttpResponse(resp)
