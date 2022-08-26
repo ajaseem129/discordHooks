9e2483b0e3ad51406b7ada8f5724a67ff9ff6d25
Drak <drak@Abduls-Air.mshome.net>
2022-08-26 21:00:26 +0530

    Rename

--- discordHooks/urls.py
+++ discordHooks/urls.py
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',include('apps.blrtrip.urls')),
+    path('',include('apps.tbag.urls')),
     ]
